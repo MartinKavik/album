67fb5daa7e703466ea4c2e999f723425767b9f15
TuntematonSotilas <gatobael@gmail.com>
2019-07-12 20:41:52 +0200

    inverse menu

--- src/components/header.rs
+++ src/components/header.rs
@@ -20,13 +20,13 @@ pub fn update(_msg: Msg, _model: &mut Model, _orders: &mut Orders<Msg>) {}
 pub fn view(_model: &Model) -> impl ElContainer<Msg> {
     header![class!("navbar main__navbar"),
         section![class!("navbar-section"),
-            a!["pictures", attrs!{At::Href => "/pictures"} ],
+            a!["albums", attrs!{At::Href => "/albums"} ],
         ],
         section![class!("navbar-center"),
             a![img![attrs!{At::Src => "front/assets/image/camera.png"}],  attrs!{At::Href => "/"} ],
         ],
-        section![class!("navbar-section"),
-            a!["albums", attrs!{At::Href => "/albums"} ],
+		section![class!("navbar-section"),
+            a!["pictures", attrs!{At::Href => "/pictures"} ],
         ]
     ]
 }
